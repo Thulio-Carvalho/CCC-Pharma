f337c59a28b122fced5819ed61e7aa2412b5e442
Thulio Carvalho <thulioicc@gmail.com>
2018-12-13 18:18:57 -0200

    added database config

---
+++ src/main/java/com/example/pharmaccc/DatabaseConfig.java
@@ -0,0 +1,9 @@
+@Configuration
+public class DatabaseConfig {
+    @Bean
+    @Primary
+    @ConfigurationProperties(prefix = "spring.datasource")
+    public DataSource dataSource() {
+        return DataSourceBuilder.create().build();
+    }
+}
