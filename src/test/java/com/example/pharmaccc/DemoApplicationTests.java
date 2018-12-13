a9f470190c2e5f1db484b4a4b86152009f48fc9c
Thulio Carvalho <thulioicc@gmail.com>
2018-12-13 18:08:24 -0200

    added backend app

--- src/test/java/com/example/pharmaccc/DemoApplicationTests.java
+++ src/test/java/com/example/pharmaccc/DemoApplicationTests.java
@@ -1,4 +1,4 @@
-package com.pharma.cccpharma;
+package com.example.pharmaccc;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class CccpharmaApplicationTests {
+public class DemoApplicationTests {
 
 	@Test
 	public void contextLoads() {
