cdbff49b486a3c0f2febb1b8f742cb5342a3e583
João Marcelo <joao.marcelo.junior@ccc.ufcg.edu.br>
2018-12-15 19:13:59 -0300

    Adicionando primeiro metodo controller produto

--- src/main/java/com/pharma/PharmaCCC/controllers/ProdutoController.java
+++ src/main/java/com/pharma/PharmaCCC/controllers/ProdutoController.java
@@ -1,17 +1,33 @@
 package com.pharma.PharmaCCC.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.pharma.PharmaCCC.services.ProdutoService;
+import com.pharma.PharmaCCC.models.Produto;
 
 @RestController
 @CrossOrigin
-@RequestMapping("/produtos")
+@RequestMapping("/produto")
 public class ProdutoController {
+	
+	@Autowired
+	ProdutoService produtoService;
 	
 	@RequestMapping(method = RequestMethod.GET)
 	public String produtoController() {
 		return "Controller Produto ok!";
+	}
+	
+	@RequestMapping(method = RequestMethod.GET)
+	public @ResponseBody List<Produto> getAllProdutos() {
+		List<Produto> produtos = produtoService.getAllClientes();
+		return produtos;
 	}
 }
